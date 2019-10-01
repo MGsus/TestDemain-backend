411bb51ffe60a13bee390f06228013054752da57
Jesús Montoya <gsus.montoya.17@gmail.com>
2019-09-30 21:55:19 -0500

    .env files deleted, NODE_ENV logic added

--- server.js
+++ server.js
@@ -22,7 +22,11 @@ app.use(morgan('dev', {stream: accessLogStream}));
 app.use(express.json());
 // app.use(bodyParser.urlencoded({extended: false}));
 
-const uri = process.env.MONGO_URI;
+let uri;
+if (process.env.NODE_ENV === 'dev')
+    uri = 'mongodb://mongodb:27017/Demain';
+if (process.env.NODE_ENV === 'production')
+    uri = 'mongodb+srv://demainRoot:demainRoot@demain-zdnjw.mongodb.net/test?retryWrites=true&w=majority';
 // const uri = 'mongodb://localhost:27017/pruebaLogin';
 
 mongoose.connect(uri, {
